e06c4217a5dc456ee522393e04fbceb4ed23fcf5
ethang404 <ethangordon25@gmail.com>
2022-10-11 18:03:47 -0500

    Setup JWT tokens, made custom user model

--- capstone/stopHopperAPI/urls.py
+++ capstone/stopHopperAPI/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import *
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)#Simple JWT import url
+
 urlpatterns = [
-    path('test/',hello_world)
+    path('test/',hello_world),
+    path('test2/',testing),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
