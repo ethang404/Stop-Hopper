a2f62e2af1c2039bc13cba30eaa8fb23352ac197
ethang404 <ethangordon25@gmail.com>
2022-10-14 15:44:40 -0500

    implemented layout for routes and private routes

--- capstone/frontend/src/App.js
+++ capstone/frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Login, Register } from "./login_Register/";
+import { NavigateHome } from "./navigation/NavigateHome";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route exact path="/Login" element={<Login />}></Route>
+					<Route exact path="/Register" element={<Register />}></Route>
+					<Route element={<PrivateRoutes />}>
+						<Route path="/Home" element={<NavigateHome />}></Route>
+					</Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
