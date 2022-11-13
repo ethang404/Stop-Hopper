4c333cd560574383c9e2c1614ba737ae0af069fe
HunHage <HunHage@gmail.com>
2022-11-13 07:05:46 -0600

    implement show/hide password functionality

--- capstone/frontend/src/login_Register/Login.js
+++ capstone/frontend/src/login_Register/Login.js
@@ -1,14 +1,18 @@
 import "../App.css";
 import "./Login.css";
 
-import { Button, Divider, List, ListItem, ListItemText, TextField } from "@mui/material";
+import {Button, IconButton, InputAdornment, TextField} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 
 export default function Login() {
 	let navigate = useNavigate();
 	const [detail, setDetail] = useState({ username: "", password: "" });
 	const [authTokens, setAuthTokens] = useState([]);
+	// Toggle Show Password Vars
+	const [showPassword, setShowPassword] = useState(false);
+	const handlePasswordToggle = () => setShowPassword(!showPassword);
 
 	async function handleSubmit() {
 		let response = await fetch("http://127.0.0.1:8000/api/token/", {
@@ -56,10 +60,25 @@ export default function Login() {
 				id="filled-basic"
 				className="TextField"
 				label="Password"
+				type={showPassword ? "text" : "password"}
 				name="password"
+				autoComplete="current-password"
 				variant="filled"
 				value={detail.password}
 				onChange={handleChange}
+				InputProps={{
+					endAdornment: (
+						<InputAdornment position={"end"}>
+							<IconButton
+								aria-label={"toggle password visibility"}
+								onClick={handlePasswordToggle}
+								onMouseDown={handlePasswordToggle}
+							>
+								{showPassword ? <Visibility /> : <VisibilityOff />}
+							</IconButton>
+						</InputAdornment>
+					)
+				}}
 			/>
 			<br/>
 			<br/>
