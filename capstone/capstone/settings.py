7adfd869b7ac4e760c5548bbf939f4b945e2b9a9
HunHage <HunHage@gmail.com>
2022-11-27 17:52:51 -0600

    extend access token lifetime to an hour

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {#Simple JWT token settings import
    
 }
 SIMPLE_JWT = {#All settings for JWT Tokens
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
