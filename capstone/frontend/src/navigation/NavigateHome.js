d6a6d711874a421bf3aee7ff051fcf8934b2ddb0
HunHage <HunHage@gmail.com>
2022-12-01 17:12:53 -0600

    fix undefined route code

--- capstone/frontend/src/navigation/NavigateHome.js
+++ capstone/frontend/src/navigation/NavigateHome.js
@@ -329,7 +329,7 @@ export default function NavigateHome() {
 		data.map(stop => { delete stop.Edit })
 
 		const response = await submitStop(data)
-		const json = response.json();
+		const json = await response.json();
 
 		if (response.ok) {
 			navigate("/RouteMenu/" + json['RouteCode']);
